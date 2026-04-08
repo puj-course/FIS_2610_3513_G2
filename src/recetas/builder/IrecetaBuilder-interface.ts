d5e45992f2ccef9c1958ac6a6e796271a678953b
Alendy-24 <dcorredor920@gmail.com>
2026-04-07 21:02:27 -0500

    refactor: Change the structure of crearReceta applying GOF patterns. Closes (#140)

---
+++ src/recetas/builder/IrecetaBuilder-interface.ts
@@ -0,0 +1,11 @@
+import { CrearRecetaDto } from '../dto/crear-receta.dto';
+import { Prisma } from '@prisma/client';
+export interface IRecetaBuilder {
+  reset(): void;
+  setDatosBase(dto: CrearRecetaDto): this;
+  setImage(url: string, buffer: Buffer): this;
+  setCategory(id: number): this;
+  setIngredients(ingredientes: any[]): this;
+  setPasos(pasos: string[]): this;
+  build(): Prisma.recetaCreateInput;
+}
