03fb603e8192b93c11994c021caddff076ad1fdb
Alendy-24 <dcorredor920@gmail.com>
2026-04-08 22:41:46 -0500

    fix: Solved some merge conflicts, it should work fine now (hopefully)

--- src/telegram/telegram.module.ts
+++ src/telegram/telegram.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TelegramService } from './telegram.service';
 import { NotificacionesFacade } from './NotificacionesFacade';
-import { NotificacionesFacade } from './NotificacionesFacade';
 
 @Module({
-  providers: [TelegramService, NotificacionesFacade],
-  exports: [TelegramService, NotificacionesFacade],
   providers: [TelegramService, NotificacionesFacade],
   exports: [TelegramService, NotificacionesFacade],
 })
