201100c640c20186b0289bcfb838b18bae557f0b
Alendy-24 <dcorredor920@gmail.com>
2026-05-01 23:15:10 -0500

    fix: add jest.config

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,12 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.spec.ts'],
+  reporters: [
+    'default',
+    ['jest-html-reporters', {
+      publicPath: './test-report',
+      filename: 'report.html',
+      openReport: false,
+    }],
+  ],
 };
