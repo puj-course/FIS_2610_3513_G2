98ec9f45432df8e5db4cd211ca15efdebb0350a5
blurryavi <salitoavilatorres@gmail.com>
2026-05-09 14:35:05 -0500

    fix: Fixing endpoint security to return 403 forbidden on request. Fixes (#197), (#198)

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -1,4 +1,4 @@
-import { Get, Controller, Post, Body, HttpCode } from '@nestjs/common';
+import { Get, Controller, Post, Body, HttpCode , ForbiddenException} from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
@@ -27,35 +27,41 @@ crearModerador(@Body() dto: RegisterDto, @Body('rolCreador') rolCreador: string)
   return this.usuariosService.crearModerador(dto, rolCreador);
 }
 
-@Post('crear-admin')
-crearAdmin(@Body() dto: RegisterDto, @Body('rolCreador') rolCreador: string) {
-  return this.usuariosService.crearAdmin(dto, rolCreador);
-}
-
-@Post('crear-verificado')
-crearChef(@Body() dto: RegisterDto, @Body('rolCreador') rolCreador: string) {
-  return this.usuariosService.crearVerificado(dto, rolCreador);
-}
-
-@Patch(':id/perfil')
-editarPerfil(
-  @Param('id', ParseIntPipe) id: number,
-  @Body() dto: EditarPerfilDto,
-) {
-  return this.usuariosService.editarPerfil(id, dto);
+  @Post('crear-admin')
+  crearAdmin(@Body() dto: RegisterDto, @Body('rolCreador') rolCreador: string) {
+    return this.usuariosService.crearAdmin(dto, rolCreador);
   }
 
-  @Get()
-    getAll(){
-      return this.usuariosService.getAll();
-    }
-// agregar endpoint para cambiar rol de usuario en el panel de crear-admin
+  @Post('crear-verificado')
+  crearChef(@Body() dto: RegisterDto, @Body('rolCreador') rolCreador: string) {
+    return this.usuariosService.crearVerificado(dto, rolCreador);
+  }
 
+  @Patch(':id/perfil')
+  editarPerfil(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: EditarPerfilDto,
+  ) {
+    return this.usuariosService.editarPerfil(id, dto);
+    }
+
+  @Get()
+  getAll(@Body('solicitanteRol') solicitanteRol: string) {
+    if (solicitanteRol !== 'admin') {
+      throw new ForbiddenException('Solo un administrador puede acceder a este recurso');
+    }
+    return this.usuariosService.getAll();
+  }
+  // agregar endpoint para cambiar rol de usuario en el panel de crear-admin
   @Patch(':id/rol')
   actualizarRol(
-    @Param('id', ParseIntPipe) id:number,
+    @Param('id', ParseIntPipe) id: number,
     @Body('rol') rol: string,
+    @Body('solicitanteRol') solicitanteRol: string,  // ← agrega esto
   ) {
+    if (solicitanteRol !== 'admin') {                // ← agrega esto
+      throw new ForbiddenException('Solo un administrador puede acceder a este recurso');
+    }
     return this.usuariosService.actualizarRol(id, rol);
   }
 }
