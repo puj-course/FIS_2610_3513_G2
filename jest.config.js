b7151b7ba8e3639b06f8ae75d46efa79e1f5acbc
Alendy-24 <dcorredor920@gmail.com>
2026-05-12 22:05:37 -0500

    Add jest config update

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,16 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/*.spec.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'text', 'text-summary'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.module.ts',
+    '!src/**/*.dto.ts',
+    '!src/main.ts',
+    '!src/**/__mocks__/**',
+  ],
   reporters: [
     'default',
     ['jest-html-reporters', {
@@ -12,5 +22,5 @@ module.exports = {
   ],
   moduleNameMapper: {
     '^~/(.*)$': '<rootDir>/src/$1',
-  }
+  },
 };
