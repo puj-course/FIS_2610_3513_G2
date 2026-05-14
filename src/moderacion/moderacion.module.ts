1f407037fad54d1a68382a55cd10531ab86d7df7
blurryavi <salitoavilatorres@gmail.com>
2026-05-13 23:34:41 -0500

    feat:Añadiendométricas de moderación. Closes #256

--- src/moderacion/moderacion.module.ts
+++ src/moderacion/moderacion.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { ModeracionController } from './moderacion.controller';
 import { ModeracionService } from './moderacion.service';
 import { AutenticacionHandler } from './handlers/autenticacion.handler';
@@ -8,18 +8,26 @@ import { NotificacionesFacade } from './facades/notificaciones.facade';
 import { PrismaService } from '../../prisma/prisma.service';
 import { UsuariosModule } from '../usuarios/usuarios.module';
 import { TelegramModule } from '../telegram/telegram.module';
-import { RecetasModule } from '../recetas/recetas.module'; // ← cambiar import
+import { RecetasModule } from '../recetas/recetas.module';
+import { MetricasMiddleware } from './metricas.middleware';
+import { MetricasService } from './metricas.service';
 
 @Module({
-  imports: [UsuariosModule, TelegramModule, RecetasModule], // ← agregar RecetasModule
+  imports: [UsuariosModule, TelegramModule, RecetasModule],
   controllers: [ModeracionController],
   providers: [
     ModeracionService,
     AutenticacionHandler,
     RolHandler,
+    MetricasMiddleware,
+    MetricasService,
     ModeraciónAccionHandler,
     NotificacionesFacade,
     PrismaService,
   ],
 })
-export class ModeracionModule {}
+export class ModeracionModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(MetricasMiddleware).forRoutes('*');
+  }
+}
